735d523d8cd625c694c492acef98707763c2136b
blubberbu <brianhandoko22@gmail.com>
2022-01-04 22:40:39 +0700

    Add EntrySeeder with dummy data

--- database/seeders/EntrySeeder.php
+++ database/seeders/EntrySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Entry;
 use Illuminate\Database\Seeder;
 
 class EntrySeeder extends Seeder
@@ -13,6 +14,29 @@ class EntrySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $userId = 2;
+        $title = 'test1';
+        $image = 'img1';
+        $body = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+
+        Entry::create([
+            'user_id' => $userId,
+            'title' => $title,
+            'image' => $image,
+            'body' => $body
+        ]);
+
+        $userId = 2;
+        $title = 'test2';
+        $image = 'img2';
+        $body = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+
+        Entry::create([
+            'user_id' => $userId,
+            'title' => $title,
+            'image' => $image,
+            'body' => $body
+        ]);
+
     }
 }

e93bfe7d8751fb0f64b0382de24eec3f931305f4
blubberbu <brianhandoko22@gmail.com>
2022-01-04 22:51:50 +0700

    Add home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    <h2>Journal</h2>
+    <div>
+        @guest
+            <a href="/login"><button>Log in</button></a>
+            <a href="/register"><button>Register</button></a>
+        @endguest
+        <div>
+            <a href="/entry"><button>New entry</button></a>
+        </div>
+        <div>
+            <a href="/entries"><button>Open Journal</button></a>
+        </div>
+
+    </div>
+</body>
+</html>

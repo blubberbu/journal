9a11cce3ec2afd705951ff62776ed979b4ac2198
Darian Jason <darianjason@gmail.com>
2022-01-06 23:12:58 +0700

    Fix typo

--- database/seeders/EntrySeeder.php
+++ database/seeders/EntrySeeder.php
@@ -16,8 +16,7 @@ class EntrySeeder extends Seeder
     {
         $titles = ['Night in Shibuya', 'Hong Kong Cityscape', 'Shibuya Glitch', 'Swiss Mountain Range', 'Bali Sunset'];
         $images = ['images/jezael-melgoza-alY6_OpdwRQ-unsplash.jpg', 'images/manson-yim-lYDOrOkR_GU-unsplash.jpg', 'images/Shibuya-glitch.jpg', 'images/etienne-bosiger-WTkUYzNCu-A-unsplash.jpg', 'images/Bali-sunset.jpg'];
-        $bodies = ['Photograph by: Jezael Melgoza (https://unsplash.com/photos/alY6_OpdwRQ)', 'Photograph by: Manson Yim (https://unsplash.com/photos/lYDOrOkR_GU)', 'Photograph by: Darian Jason', 'Photograph by: 
-        Etienne Bösiger (https://unsplash.com/photos/WTkUYzNCu-A)', 'Photograph by: Darian Jason, 100% unedited'];
+        $bodies = ['Photograph by: Jezael Melgoza (https://unsplash.com/photos/alY6_OpdwRQ)', 'Photograph by: Manson Yim (https://unsplash.com/photos/lYDOrOkR_GU)', 'Photograph by: Darian Jason', 'Photograph by: Etienne Bösiger (https://unsplash.com/photos/WTkUYzNCu-A)', 'Photograph by: Darian Jason, 100% unedited'];
 
         //dummy data
         for ($i=0; $i < count($titles); $i++) { 
